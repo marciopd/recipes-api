a7abca767133d42f0d4d081281072d264d71284d
Márcio Paixão Dantas <marciopd@gmail.com>
2023-06-20 07:44:02 +0200

    Configuring Swagger for JWT authorization.

--- src/main/java/com/marciopd/recipesapi/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/marciopd/recipesapi/configuration/security/WebSecurityConfig.java
@@ -11,12 +11,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+
+    private static final String[] SWAGGER_UI_RESOURCES = {
+            "/v3/api-docs/**",
+            "/configuration/ui/**",
+            "/swagger-resources/**",
+            "/configuration/security/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**"};
 
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
@@ -34,20 +40,11 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(registry ->
                         registry.requestMatchers(HttpMethod.POST, "/users", "/tokens").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/recipes/**", "/tags/**").permitAll()
+                                .requestMatchers(SWAGGER_UI_RESOURCES).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(configure -> configure.authenticationEntryPoint(authenticationEntryPoint))
                 .addFilterBefore(authenticationRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
     }
 }
