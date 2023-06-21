aa0b4795e2a1e3b44c3e07facecc06f6f0669ba4
Márcio Paixão Dantas <marciopd@gmail.com>
2023-06-22 00:05:36 +0200

    Unit tests for login use case and recipe data change validator.

--- src/test/java/com/marciopd/recipesapi/business/impl/RecipeDataChangeUserValidatorImplTest.java
+++ src/test/java/com/marciopd/recipesapi/business/impl/RecipeDataChangeUserValidatorImplTest.java
@@ -1,5 +1,6 @@
 package com.marciopd.recipesapi.business.impl;
 
+import com.marciopd.recipesapi.business.exception.UnauthorizedDataAccessException;
 import com.marciopd.recipesapi.configuration.security.token.AccessToken;
 import com.marciopd.recipesapi.persistence.entity.RecipeEntity;
 import com.marciopd.recipesapi.persistence.entity.UserEntity;
@@ -9,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -41,6 +44,22 @@ class RecipeDataChangeUserValidatorImplTest {
                 .user(UserEntity.builder().id(userId).build())
                 .build();
         recipeDataChangeUserValidator.validateIfCurrentUserCanChangeRecipe(recipeEntity);
+
+        verify(currentRequestUserTokenMock).hasRole("ADMIN");
+        verify(currentRequestUserTokenMock).getUserId();
+    }
+
+    @Test
+    void validate_shouldThrowError_whenUserIsNotAdminOrRecipeOwner() {
+        when(currentRequestUserTokenMock.hasRole("ADMIN")).thenReturn(false);
+        when(currentRequestUserTokenMock.getUserId()).thenReturn(1L);
+
+        RecipeEntity recipeEntity = RecipeEntity.builder()
+                .user(UserEntity.builder().id(55L).build())
+                .build();
+        UnauthorizedDataAccessException exception = assertThrows(UnauthorizedDataAccessException.class,
+                () -> recipeDataChangeUserValidator.validateIfCurrentUserCanChangeRecipe(recipeEntity));
+        assertEquals("LOGGED_USER_NOT_OWNER_RECIPE", exception.getReason());
 
         verify(currentRequestUserTokenMock).hasRole("ADMIN");
         verify(currentRequestUserTokenMock).getUserId();
